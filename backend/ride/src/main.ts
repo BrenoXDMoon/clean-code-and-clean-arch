af199a242c659e7eb03206aa4b862100db5b59b7
Breno Gabriel <brenogabriel19@gmail.com>
2023-11-28 16:04:03 -0300

    Altera os tests para tests Web

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -1,7 +1,24 @@
 import crypto from "crypto";
 import pgp from "pg-promise";
+import express, {Request, Response} from "express";
 
-export function validateCpf (cpf: string) {
+const app = express();
+app.use(express.json());
+app.listen(3000);
+
+app.post("/signup", async function (request: Request, response: Response){
+	const input = request.body;
+	const output = await signup(input);
+	response.json(output);
+});
+
+app.get("/accounts/:accountId", async function (request: Request, response: Response){
+	const accountId = request.params.accountId;
+	const output = await getAccount(accountId);
+    response.json(output);
+});
+
+function validateCpf (cpf: string) {
 	if (!cpf) return false;
 	cpf = clean(cpf);
 	if (isInvalidLength(cpf)) return false;
@@ -36,7 +53,7 @@ function extractCheckDigit (cpf: string) {
 	return cpf.slice(9);
 }
 
-export async function signup (input: any): Promise<any> {
+async function signup (input: any): Promise<any> {
 	const connection = getConnection();
 	try {
 		const accountId = crypto.randomUUID();
@@ -67,7 +84,7 @@ function isInvalidCarPlate (carPlate: string) {
 	return !carPlate.match(/[A-Z]{3}[0-9]{4}/);
 }
 
-export async function getAccount (accountId: string) {
+async function getAccount (accountId: string) {
 	const connection = getConnection();
 	const [account] = await connection.query("select * from cccat14.account where account_id = $1", [accountId]);
 	await connection.$pool.end();
